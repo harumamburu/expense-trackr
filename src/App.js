08bc3ba96882b69bdfddc23ff4dd2211fd50aa8b
harumamburu <nikolay.kovalev92@gmail.com>
2022-04-07 17:54:14 +0300

    Add props to ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+export default function App() {
+  const expenses = [
+    { title: "MacBook", amount: "$100500", date: new Date(2022, 3, 4) },
+    { title: "Wape thingy", amount: "$15", date: new Date(2022, 3, 5) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      { expenses.map(element => 
+          <ExpenseItem title={element.title}
+                       amount={element.amount}
+                       date={element.date} />
+        )
+      }
     </div>
   );
 }
-
-export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   return (
-    <div className='expense-item'>
-      <div>April 4th 2022</div>
-      <div className='expense-item__description'>
-        <h2>MacBook</h2>
-        <div className='expense-item__price'>$100500</div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
