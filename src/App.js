f45f098aa308f160da8fbb144c11a036ae3af7e7
harumamburu <nikolay.kovalev92@gmail.com>
2022-04-08 10:55:50 +0300

    Add more test data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import ExpensesList from "./components/ExpensesList";
 
 export default function App() {
   const expenses = [
-    { title: "MacBook", amount: "$100500", date: new Date(2022, 3, 4) },
+    { title: "React text book", amount: "$20", date: new Date(2021, 11, 30) },
+    { title: "MacBook", amount: "$100500", date: new Date(2022, 2, 30) },
     { title: "Wape thingy", amount: "$15", date: new Date(2022, 3, 5) },
+    { title: "Checked shirt", amount: "$105", date: new Date(2022, 3, 6) }
   ];
 
   return (
