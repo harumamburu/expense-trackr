0653ef7987557531f0f2c1ef87a88e63c6ce99fd
harumamburu <nikolay.kovalev92@gmail.com>
2022-04-11 11:46:48 +0300

    Refactor expenses list
    To make JSx more readable and concise

--- src/components/expenses/ExpensesList.js
+++ src/components/expenses/ExpensesList.js
@@ -7,26 +7,28 @@ import "./ExpensesList.css";
 
 const ExpensesList = props => {
   const [filterYear, setFilterYear] = useState("");
-  const dateFilterChangeHandler = newFilterDate => setFilterYear(newFilterDate);
+  const dateFilterChangeHandler = newFilterYear => setFilterYear(newFilterYear);
 
-  return (
+  const filteredExpenses = props.expenses
+    .filter(expense =>
+      filterYear ? expense.date.getFullYear().toString() === filterYear : true
+    )
+    .sort((a, b) => new Date(a.date) - new Date(b.date));
+
+    return (
     <Card className="expenses">
-      <ExpensesFilter selected={filterYear} onFilterChange={dateFilterChangeHandler} />
-      {props.expenses
-        .filter(expense =>
-          filterYear
-            ? expense.date.getFullYear().toString() === filterYear
-            : true
-        )
-        .sort((a, b) => new Date(a.date) - new Date(b.date))
-        .map(expense =>
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        )}
+      <ExpensesFilter
+        selected={filterYear}
+        onFilterChange={dateFilterChangeHandler}
+      />
+      {filteredExpenses.map(expense =>
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      )}
     </Card>
   );
 };
