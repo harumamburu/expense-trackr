1074f351a73a65692f557f8d961492378842b365
harumamburu <nikolay.kovalev92@gmail.com>
2022-04-11 10:44:58 +0300

    Add a callback to update App state properly

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { useState } from "react";
 import ExpensesList from "./components/expenses/ExpensesList";
 import NewExpense from "./components/expenses/newexpense/NewExpense";
 
-const App = () => {
-  const [expenses, setExpenses] = useState([
-    { title: "React text book", amount: "$20", date: new Date(2021, 11, 30) },
-    { title: "MacBook", amount: "$100500", date: new Date(2022, 2, 30) },
-    { title: "Wape thingy", amount: "$15", date: new Date(2022, 3, 5) },
-    { title: "Checked shirt", amount: "$105", date: new Date(2022, 3, 6) }
-  ]);
+const INIT_DATA = [
+  { title: "React text book", amount: "$20", date: new Date(2021, 11, 30) },
+  { title: "MacBook", amount: "$100500", date: new Date(2022, 2, 30) },
+  { title: "Wape thingy", amount: "$15", date: new Date(2022, 3, 5) },
+  { title: "Checked shirt", amount: "$105", date: new Date(2022, 3, 6) }
+];
 
-  const addExpenseHandler = expense => setExpenses([...expenses, expense]);
+const App = () => {
+  const [expenses, setExpenses] = useState(INIT_DATA);
+
+  const addExpenseHandler = expense => setExpenses(oldExpenses => [...oldExpenses, expense]);
 
   return (
     <div>
